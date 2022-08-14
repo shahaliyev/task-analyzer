5612dfa2a1f75c9aae85ee7c5a9ac911cee1636c
Shahaliyev <shahaliyev99@gmail.com>
2022-08-14 19:32:35 +0400

    added cmd line argument

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from utils import get_tasks_from_excel, split_tasks
 from report import get_report
+import sys, os
 
 
 if __name__ == '__main__': 
-    all_tasks = get_tasks_from_excel('./files/Tasks.xlsx')
+    filepath = os.path.join('./files', sys.argv[1])
+    all_tasks = get_tasks_from_excel(filepath)
     tasks, completed_tasks = split_tasks(all_tasks)
     get_report(tasks)
 
